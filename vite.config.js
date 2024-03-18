65cbbb19f9c6de749d18a22461d6c59469610349
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-03-18 22:23:59 +0200

    Initial project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/vite-react-routes-tmp/"
+    base: "/"
 })
 
 // import.meta.env.BASE_URL - getting url from base: "/vite-react-routes-tmp/"
